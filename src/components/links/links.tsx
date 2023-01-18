df0939b0a0ac00ab8e84dd6a355d2d956839f793
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-01-18 06:49:41 +0100

    feat: links component

---
+++ src/components/links/links.tsx
@@ -0,0 +1,43 @@
+import "./links.css";
+
+export function Links() {
+  return (
+    <div className="links">
+      <a
+        href="https://github.com/mirayatech"
+        target={"_blank"}
+        className="link"
+      >
+        GitHub
+      </a>
+      <a
+        href="https://www.instagram.com/mirayatech/"
+        target={"_blank"}
+        className="link"
+      >
+        Instagram
+      </a>
+      <a
+        href="https://www.tiktok.com/@mirayatech"
+        target={"_blank"}
+        className="link"
+      >
+        TikTok
+      </a>{" "}
+      <a
+        href="https://www.youtube.com/@mirayatech."
+        target={"_blank"}
+        className="link"
+      >
+        YouTube
+      </a>
+      <a
+        href="https://twitter.com/mirayadev"
+        target={"_blank"}
+        className="link"
+      >
+        Twitter
+      </a>
+    </div>
+  );
+}
